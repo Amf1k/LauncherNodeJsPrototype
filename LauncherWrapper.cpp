ca0ee31530935afdaf3911ef09d3ede04e3a24dc
Vladimir Zryachih <v.zryachih@xsolla.com>
2020-11-19 16:30:13 +0500

    fix

--- LauncherWrapper.cpp
+++ LauncherWrapper.cpp
@@ -21,18 +21,21 @@ amf::LauncherWrapper::LauncherWrapper(const Napi::CallbackInfo& info)
     : Napi::ObjectWrap<LauncherWrapper>(info), _pLauncher(new Launcher()) {}
 
 Napi::Value amf::LauncherWrapper::exec(const Napi::CallbackInfo& info) {
-  return Napi::Number::New(info.Env(), 1);
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+  int exitCode = _pLauncher->exec();
+  return Napi::Number::New(env, exitCode);
 }
 
 Napi::Value amf::LauncherWrapper::startGame(const Napi::CallbackInfo& info) {
-  return Napi::Number::New(info.Env(), 1);
+  return info.Env().Null();
 }
 
 Napi::Value amf::LauncherWrapper::stopGame(const Napi::CallbackInfo& info) {
-  return Napi::Number::New(info.Env(), 1);
+  return info.Env().Null();
 }
 
 Napi::Value amf::LauncherWrapper::games(const Napi::CallbackInfo& info) {
   auto games = _pLauncher->games();
-  return Napi::String::New(info.Env(), games[1]->name().toStdString());
+  return Napi::String::New(info.Env(), games.first()->name().toStdString());
 }
